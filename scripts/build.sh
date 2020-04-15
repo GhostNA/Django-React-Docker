9c0b53d081e8d495bbe72d365a9875ca94e95775
GhostNA <achillesniko@gmail.com>
2020-04-15 22:50:04 +0300

    init

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+docker-compose \
+-f docker-compose.admin.yml \
+-f docker-compose.base-images.yml \
+-f docker-compose.build.yml \
+-f docker-compose.command.yml \
+-f docker-compose.depends.yml \
+-f docker-compose.env.yml \
+-f docker-compose.networks.yml \
+-f docker-compose.ports.yml \
+-f docker-compose.volumes.yml \
+config > docker-stack.yml
+
+#docker-compose -f docker-stack.yml build
+#docker-compose -f docker-stack.yml up -d
